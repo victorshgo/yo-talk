97e3785a8f6f6d33820efd941408ac158901c71a
Victor Hugo <ter_victor.hugo@tempotem.com.br>
2021-06-14 01:52:59 -0300

    Adding keys.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
-    ACCOUNT_SID: "",
-    API_KEY: "",
-    API_SECRET: ""
+    ACCOUNT_SID: "AC38b3c9b5fa7a924bfaf3f0811fd4e5bb",
+    API_KEY: "SKf4e40776608469c77f34c5dea0b84480",
+    API_SECRET: "yVbLFZCysfxTxzBGlz8u0pkNNFMdHoy1"
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
